d218292655bf281e30e4feb09e0735778df7b2de
Lin20050804 <jackylin0036@gmail.com>
2024-11-29 23:30:14 -0500

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,7 +2,7 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: 'Hailey, 早上好',
     desc: `
       **{{date.DATA}}**
       
@@ -20,20 +20,6 @@ const TEMPLATE_CONFIG = [
       
       风级: {{wind_scale.DATA}}
       
-      {{comprehensive_horoscope.DATA}}
-      
-      ---
-      
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
-      
-      ---
-      
-      {{moment_copyrighting.DATA}}
-      
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
-    `
   },
   {
     id: '0002',
